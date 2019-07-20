261a5fddedafc7b3b789b9ec5a43f3d7a26d078b
Alexander <mcsibsart@gmail.com>
2019-07-20 00:49:22 -0400

    Safe mode was pushed later so the tag file could be created.

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,13 @@ fn main() -> Result<(), Error> {
     Config::check_config()?;
     let mut config = Config::get_config()?;
 
-    // Creates connector to prepare for downloading posts.
-    let mut connector = EsixWebConnector::new(&mut config);
-    connector.should_enter_safe_mode();
-
     // Create tag if it doesn't exist, then parse it.
     let tag_path = Path::new(TAG_NAME);
     create_tag_file(&tag_path)?;
+
+    // Creates connector to prepare for downloading posts.
+    let mut connector = EsixWebConnector::new(&mut config);
+    connector.should_enter_safe_mode();
 
     // Parse tag file
     let groups = parse_tag_file(&tag_path)?;
